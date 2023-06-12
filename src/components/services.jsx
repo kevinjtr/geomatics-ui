53c67747e4f4d79f22f56294ff791793720b5922
JMerritt117 <jmerritt6500@floridapoly.edu>
2023-06-12 12:25:22 -0400

    updated services to read from json
    can specify a type of top left or right for the orientation of the image compared to text in the json

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -2,37 +2,50 @@ import React from "react";
 
 export const Services = (props) => {
   return (
-    <div className="text-center"> 
-    <div className="container">
-      <div style={{paddingTop:"60px"}}>
-        <img src ="../img/portfolio/01-small.jpg" alt="title" style={{float:"left"}}/>
-        <h2>Title 1</h2>
-        <p style={{textAlign:"center", paddingLeft:"50px"}}>
-          Random Paragraph about the first support service offered. Honestly on  a theoretical level, I could make this dynamic just like the GisProjects page, but I wanna set up design visually first.
-
-        </p>
-      </div>
-    </div>
-    <div className="container">
-      <div style={{paddingTop:"60px"}}>
-        <img src ="../img/portfolio/02-small.jpg" alt="title" style={{float:"right"}}/>
-        <h2>Title 2</h2>
-        <p style={{textAlign:"center", paddingLeft:"50px"}}>
-          I'd want more spacing between these pages for sure.
-        </p>
-      </div>
-    </div>
-    <div className="container">
-      <div style={{paddingTop:"60px"}}>
-        
-        <h2>Title 3</h2>
-        <p style={{textAlign:"center", paddingLeft:"50px"}}>
-          i think it would be nice if the photos varied in orientation as well. Will talk details with Kevin and co.
-        </p>
-        <img src ="../img/portfolio/03-small.jpg" alt="title" style={{}}/>
-      </div>
-    </div>
+    <div className="text-center">
+      {props.data
+      ? props.data.map((d,i) => (
+        <div
+          key={`${d.title}-${i}`}
+          >
+            <div>
+              {
+                d.type === "left" ?
+                <div className="container">
+                <div style={{paddingTop:"100px"}}>
+                <img src ={d.image} alt="title" style={{float:"left"}}/>
+                <h2>{d.name}</h2>
+                <p style={{textAlign:"center", paddingLeft:"50px"}}>
+                {d.text}
+                </p>
+                </div>
+                </div>
+                : d.type ===  "right" ?
+                <div className="container">
+                <div style={{paddingTop:"100px"}}>
+                <img src ={d.image} alt="title" style={{float:"right"}}/>
+                <h2>{d.name}</h2>
+                <p style={{textAlign:"center", paddingLeft:"50px"}}>
+                {d.text}
+                </p>
+                </div>
+                </div>
+                : d.type === "top" ? 
+                <div className="container">
+                <div style={{paddingTop:"100px"}}>
+                <h2>{d.name}</h2>
+                <p style={{textAlign:"center", paddingLeft:"50px"}}>
+                {d.text}
+                </p>
+                <img src ={d.image} alt="title" />
+                </div>
+                </div>
+                : null
+              }
+            </div>
+            </div>
+      )): "Loading..."} 
+   </div>
     
-    </div>
   );
 };
