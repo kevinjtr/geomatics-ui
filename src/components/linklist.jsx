93cec834e34890ecfb13623d6349883503af930d
JMerritt117 <jmerritt6500@floridapoly.edu>
2023-06-13 16:37:02 -0400

    styled link list more

--- src/components/linklist.jsx
+++ src/components/linklist.jsx
@@ -5,6 +5,7 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
 import { Divider } from "@mui/material";
 
 export const BasicList = (props) => {
@@ -15,8 +16,13 @@ export const BasicList = (props) => {
       </div>
     <Box sx={{ width: '100%', display:"flex", justifyContent:"center", paddingBottom:"30px"}}>
       
-      <List >
-      <h4 style={{backgroundColor:"#125780", color:"white"}}>Example Category 1</h4>
+      <List inset sx={{ width: '100%', maxWidth: 250, bgcolor: '#125780' }}
+      component="nav"
+      subheader={
+        <ListSubheader component="h4" sx={{bgcolor: "#125780", color:"white", fontSize:"20px"}}>
+          Example Category 1
+        </ListSubheader>
+      }>
         {props.data
         ? props.data.map((d,i) => (
           <div
@@ -37,8 +43,12 @@ export const BasicList = (props) => {
       :"Loading"}
       </List>
       <Divider orientation="vertical"/>
-      <List >
-      <h4 style={{backgroundColor:"#8a2b1e", color:"white"}}>Example Category 2</h4>
+      <List inset sx={{ width: '100%', maxWidth: 250, bgcolor: '#8a2b1e' }}
+      component="nav"
+      subheader={
+        <ListSubheader component="h4" sx={{bgcolor: "#8a2b1e", color:"white",fontSize:"20px"}}>
+          Example Category 2
+        </ListSubheader>} >
         {props.data
         ? props.data.map((d,i) => (
           <div 
@@ -59,8 +69,12 @@ export const BasicList = (props) => {
       :"Loading"}
       </List>
       <Divider orientation="vertical"/>
-      <List>
-        <h4 style={{backgroundColor:"#a68716", color:"white"}}>Example Category 3</h4>
+      <List inset sx={{ width: '100%', maxWidth: 250, bgcolor: '#a68716' }}
+      component="nav"
+      subheader={
+        <ListSubheader component="h4" sx={{bgcolor: "#a68716", color:"white", fontSize:"20px"}}>
+          Example Category 3
+        </ListSubheader>} >
         {props.data
         ? props.data.map((d,i) => (
           <div 
