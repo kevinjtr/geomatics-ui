3830fe5c7d825555776fbb3c7b08fe995fcbbad1
JMerritt117 <jmerritt6500@floridapoly.edu>
2023-07-17 13:15:09 -0400

    small changes to support so it has hrefs

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -13,31 +13,36 @@ export const Services = (props) => {
                 d.type === "left" ?
                 <div className="container">
                 <div style={{paddingTop:"100px"}}>
+                <a href={d.link}>
                 <img src ={d.image} alt="title" style={{float:"left"}}/>
                 <h2>{d.name}</h2>
                 <p style={{textAlign:"center", paddingLeft:"50px"}}>
                 {d.text}
                 </p>
+                </a>
                 </div>
                 </div>
                 : d.type ===  "right" ?
                 <div className="container">
                 <div style={{paddingTop:"100px"}}>
+                <a href={d.link}>
                 <img src ={d.image} alt="title" style={{float:"right"}}/>
                 <h2>{d.name}</h2>
                 <p style={{textAlign:"center", paddingLeft:"50px"}}>
                 {d.text}
                 </p>
+                </a>
                 </div>
                 </div>
                 : d.type === "top" ? 
                 <div className="container">
                 <div style={{paddingTop:"100px"}}>
+                <a href={d.link}>
                 <h2>{d.name}</h2>
                 <p style={{textAlign:"center", paddingLeft:"50px"}}>
                 {d.text}
                 </p>
-                <img src ={d.image} alt="title" />
+                <img src ={d.image} alt="title" /></a>
                 </div>
                 </div>
                 : null
